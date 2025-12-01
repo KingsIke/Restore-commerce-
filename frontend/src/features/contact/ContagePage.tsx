030939d9c346cc765fbaf113309e5e4757ed6d4e
ogbonnaya kings <ogbonnayakingsike@gmail.com>
2025-12-01 13:06:13 -0800

    Redux action

--- frontend/src/features/contact/ContagePage.tsx
+++ frontend/src/features/contact/ContagePage.tsx
@@ -1,5 +1,33 @@
+import { useDispatch, useSelector } from "react-redux";
+import { DECREMENT, INCREMENT, type CounterState } from "./counterReducer";
+import { Button, ButtonGroup, Typography } from "@mui/material";
+
 export const ContagePage = () => {
+  const data = useSelector((state: CounterState) => state.data);
+  const dispatch = useDispatch()
+  console.log("Contage Page Rendered with data:", data);
   return (
-    <div>ContagePage</div>
+    <><Typography variant="h2">Contage Page Data</Typography>
+    <Typography variant="h4">{data}</Typography>
+    <ButtonGroup>
+      <Button onClick={() => dispatch(DECREMENT()) }
+      color= "error">
+        Decrement
+
+      </Button>
+      <Button onClick={() => dispatch(INCREMENT()) }
+      color= "secondary">
+        Increment
+
+      </Button>
+      <Button onClick={() => dispatch(INCREMENT(5)) }
+      color= "secondary">
+        Increment by 5
+
+      </Button>
+    </ButtonGroup>
+
+
+    </>
   )
 }
--- frontend/src/features/contact/counterReducer.ts
+++ frontend/src/features/contact/counterReducer.ts
@@ -5,8 +5,35 @@ export const initialState: CounterState = {
     data: 41,
 };
 
+export  function INCREMENT(amount = 1){
+    return{
+        type:"INCREMENT",
+        payload: amount
+    }
+}
+export function DECREMENT(amount = 1){
+    return{
+        type:"DECREMENT",
+        payload: amount
+    }
+}
+
 export default function counterReducer(
-    state  = initialState
+    state  = initialState, action: { type: string, payload: number}
 ){
-    return state;
+    switch(action.type){
+        case "INCREMENT":
+            return {
+                ...state,
+                data: state.data + action.payload
+            }
+        case "DECREMENT": 
+            return {
+                ...state,
+                data: state.data - action.payload
+            }
+        default:
+            return state;
+    }
+ 
 }
